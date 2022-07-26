485a4e9c9b395d0cbe88b1bf873af6cd9dc7f200
luojiyin <luojiyin@hotmail.com>
2022-07-26 15:21:49 +0800

    feat: creat table

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+// { "date":"20220726" ,'startTime':'9:00', "Duration":"15min","partner": A, "startup":"1"}
+const planData= [];
 
 
 
 app.get('/plan', (req, res) => {
-  res.send({'Hello World!':'sdfdf'})
+  res.send(planData)
 })
 
 app.listen(port, () => {
