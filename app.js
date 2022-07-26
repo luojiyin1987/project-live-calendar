4975c18bc98969ee67673d27341c6701226d4c6f
luojiyin <luojiyin@hotmail.com>
2022-07-26 16:39:35 +0800

    to auto create table

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+
 const app = express()
 const port = 3000
+
+
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// { "date":"20220726" ,'startTime':'9:00', "Duration":"15min","partner": A, "startup":"1"}
+// { "date":"20220726" ,'startTime':'9:00', "Duration":"15min","partner": "A", "startup":"1"}
 const planData= [];
 
+const partners = ['A', 'B', 'C', 'D']
+const startups =['1','2', '3','4', '5']
+const time = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
+
+for(let i=0; i<time.length; i++) {
+    for (let k=0; k<4; k++ ) {
+        for(let j=0; j< partners.length ; j++) {
+                const temp = {"data":"20220726", 'startTime': time[i] + ':' + (k * 15).toString().padStart(2,'0'), "Duration": "15min", "partner":partners[j] }
+                planData.push(temp)
+        }
+    }
+} 
 
 
 app.get('/plan', (req, res) => {
   res.send(planData)
 })
 
+app.post('/addPlan', (req, res)=>{
+    res.send(addPlan(req))
+})
+
+app.post('/delPlan',  (res, res) =>{
+    res.send(delPlan(res))
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+function addPlan(data) {
+
+} 
+
+function delPlan(data) {
+    
+}
--- public/script.js
+++ public/script.js
@@ -1,20 +1,38 @@
 const selectA = document.getElementById('selectA')
 const selectB = document.getElementById('selectB')
 
-selectA.addEventListener('change', (e)=> {
+
+selectA.addEventListener('change', (e) => {
     console.log(e.target.value)
     getPlan()
 })
 
-selectB.addEventListener('change', (e)=> {
+selectB.addEventListener('change', (e) => {
     console.log(e.target.value)
     getPlan()
 })
 
 
-async  function getPlan(){
+async function getPlan() {
     const url = './plan'
     const res = await fetch(url)
     const data = await res.json()
     console.log('getPlan', data);
+}
+
+function showPlan(data) {
+    const table = document.createElement('table')
+
+    let tableHTML =   `
+        <caption>
+        日程表
+    </caption>
+    <tr>
+        <th>时间( 7月26 )</th>
+        <th>合伙人 A</th>
+        <th>合伙人 B</th>
+        <th>合伙人 C</th>
+        <th>合伙人 D</th>
+    </tr>
+    `
 }
